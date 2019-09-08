4def48f87d5228ece1bccdda1f5b6fb3442ae46b
Ben Brougher <moutansos@gmail.ocm>
2019-09-07 18:48:04 -0700

    change tag lines and site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `benbrougher.tech`,
+    author: `Ben Brougher`,
+    description: `A Place to Share and Collaborate with Fellow Developers`,
+    siteUrl: `https://benbrougher.tech/`
   },
   plugins: [
     {
--- src/components/bio.js
+++ src/components/bio.js
@@ -54,12 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Currently a Work-In-Progress.
+        Written by <strong>{author}</strong> who lives and works in the Pacific 
+        Northwest building useful things.
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
       </p>
     </div>
   )
