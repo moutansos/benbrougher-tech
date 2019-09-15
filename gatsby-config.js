174970709a9d73b424efe37b4cd1d9c2defa2524
Ben Brougher <moutansos@gmail.ocm>
2019-09-14 22:07:17 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-147965258-1",
       },
     },
     `gatsby-plugin-feed`,
