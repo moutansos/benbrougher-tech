bbe6aaeaf6805f554d7266d634476725e91e81c6
Ben Brougher <moutansos@gmail.com>
2022-09-18 11:21:15 -0700

    see if giscus works on the server

---
+++ src/components/Comments.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Giscus from "@giscus/svelte";
+</script>
+
+<Giscus id="comments"
+    repo="moutansos/benbrougher-tech"
+    repoId="MDEwOlJlcG9zaXRvcnkyMDcwNDQ2ODY"
+    category="Comments"
+    categoryId="DIC_kwDODFdATs4CRfjk"
+    mapping="og:title"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="dark"
+    lang="en"
+    strict="0"/>
